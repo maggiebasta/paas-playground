2ec35cd200328ee52231026acfe57ac33ef7dac6
maggiebasta@yahoo.com <maggiebasta@yahoo.com>
2023-03-15 11:52:00 -0700

    fix chat clearing bug

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from flask import Flask, render_template, request, url_for, flash, redirect, ses
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
+messages = [{"role": "system", "content": "you are a helpful assistant"},]
 
 @app.route('/',  methods=('GET', 'POST'))
 def index():
-    session["messages"] = []
     if request.method == 'POST':
         content = request.form['content']
         if not content:
             flash('Open AI Key is required!')
+
         else:
             try:
                 openai.ChatCompletion.create(
@@ -25,7 +26,7 @@ def index():
                 session["key"] = content
                 return redirect(url_for('create'))
 
-    return render_template('index.html', messages=session["messages"])
+    return render_template('index.html', messages=messages)
 
 
 @app.route('/create/', methods=('GET', 'POST'))
@@ -34,7 +35,7 @@ def create():
         return redirect(url_for('index'))
     if request.method == 'POST':
         if request.form['submit_button'] == "Clear":
-            session["messages"] = []
+            messages.clear()
             session["key"] = None
             return redirect(url_for('index'))
         content = request.form['content']
@@ -42,15 +43,15 @@ def create():
             flash('Content is required!')
                 
         else:
-            session["messages"].append({"role": "user",'content': content})
+            messages.append({"role": "user",'content': content})
             system_response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=session["messages"],
+                messages=messages,
                 api_key=session["key"],
             )
-            session["messages"].append(
+            messages.append(
                 {"role": "system", "content": system_response['choices'][0]['message']['content']}
             )
-            return render_template('create.html', messages=session["messages"])
+            return render_template('create.html', messages=messages[1:])
 
     return render_template('create.html')
