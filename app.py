555ad2f2f93c5fe6cad82318f7da6ca86712cfc5
maggiebasta@yahoo.com <maggiebasta@yahoo.com>
2023-03-14 17:57:35 -0700

    add clear and key check

--- app.py
+++ app.py
@@ -4,20 +4,28 @@ from flask import Flask, render_template, request, url_for, flash, redirect, ses
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-messages = []
 
 @app.route('/',  methods=('GET', 'POST'))
 def index():
+    session["messages"] = []
     if request.method == 'POST':
         content = request.form['content']
         if not content:
             flash('Open AI Key is required!')
-
         else:
-            session["key"] = content
-            return redirect(url_for('create'))
+            try:
+                openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[{"role": "user", "content": "hi"}],
+                    api_key=content,
+                )
+            except openai.error.AuthenticationError:
+                flash('Invalid API key!')
+            else:
+                session["key"] = content
+                return redirect(url_for('create'))
 
-    return render_template('index.html', messages=messages)
+    return render_template('index.html', messages=session["messages"])
 
 
 @app.route('/create/', methods=('GET', 'POST'))
@@ -25,20 +33,24 @@ def create():
     if not session.get("key"):
         return redirect(url_for('index'))
     if request.method == 'POST':
+        if request.form['submit_button'] == "Clear":
+            session["messages"] = []
+            session["key"] = None
+            return redirect(url_for('index'))
         content = request.form['content']
         if not content:
             flash('Content is required!')
                 
         else:
-            messages.append({"role": "user",'content': content})
+            session["messages"].append({"role": "user",'content': content})
             system_response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=messages,
+                messages=session["messages"],
                 api_key=session["key"],
             )
-            messages.append(
+            session["messages"].append(
                 {"role": "system", "content": system_response['choices'][0]['message']['content']}
             )
-            return render_template('create.html', messages=messages)
+            return render_template('create.html', messages=session["messages"])
 
     return render_template('create.html')
